e91b0d95d927133ed910640db13e1705864dcebf
Hazem <poppinghazem3@gmail.com>
2025-05-27 14:11:40 +0300

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,108 @@
+import 'package:engineer_management_system/pages/admin/admin_clients_page.dart';
+import 'package:engineer_management_system/pages/admin/admin_employees_page.dart';
+import 'package:engineer_management_system/pages/admin/admin_engineers_page.dart';
+import 'package:engineer_management_system/pages/admin/admin_project_details_page.dart';
+import 'package:engineer_management_system/pages/admin/admin_projects_page.dart';
+import 'package:engineer_management_system/pages/engineer/edit_phase_page.dart';
+import 'package:engineer_management_system/pages/engineer/project_details_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+// استورد صفحاتك:
+import 'pages/auth/login_page.dart';
+import 'pages/admin/admin_dashboard.dart';
+import 'pages/engineer/engineer_home.dart';
+import 'pages/client/client_home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Electro App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/projectDetails': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as String;
+          return ProjectDetailsPage(projectId: args);
+        },
+        '/admin': (context) => const AdminDashboard(),
+        '/admin/engineers': (context) => const AdminEngineersPage(),
+        '/admin/clients': (context) => const AdminClientsPage(),
+        '/admin/employees': (context) => const AdminEmployeesPage(),
+        '/admin/projectDetails': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as String;
+          return AdminProjectDetailsPage(projectId: args);
+        },
+        '/admin/projects': (context) => const AdminProjectsPage(),
+        '/engineer': (context) => const EngineerHome(),
+        '/client': (context) => const ClientHome(),
+        '/editPhase': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+          return EditPhasePage(
+            projectId: args['projectId'],
+            phaseId: args['phaseId'],
+            phaseData: args['phaseData'],
+          );
+        }
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // المستخدم غير مسجل دخول
+        if (!snapshot.hasData) {
+          return const LoginPage();
+        }
+
+        // المستخدم مسجل دخول، نتحقق من نوعه
+        return FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('users')
+              .doc(snapshot.data!.uid)
+              .get(),
+          builder: (context, userSnapshot) {
+            if (!userSnapshot.hasData) {
+              return const Scaffold(body: Center(child: CircularProgressIndicator()));
+            }
+
+            final role = userSnapshot.data!['role'];
+
+            if (role == 'admin') {
+              return const AdminDashboard();
+            } else if (role == 'engineer') {
+              return const EngineerHome();
+            } else if (role == 'client') {
+              return const ClientHome();
+            } else {
+              return const Scaffold(body: Center(child: Text('نوع المستخدم غير معروف')));
+            }
+          },
+        );
+      },
     );
   }
 }
