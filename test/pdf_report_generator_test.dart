0dcfec92eb0d080bdac668afac7c8dab0609d1e2
H_Ghurbani <52192905+HGhurbani@users.noreply.github.com>
2025-07-08 19:20:24 +0300

    Reduce PDF image memory usage

--- test/pdf_report_generator_test.dart
+++ test/pdf_report_generator_test.dart
@@ -4,10 +4,10 @@ import 'package:engineer_management_system/utils/pdf_report_generator.dart';
 import 'package:image/image.dart' as img;
 
 void main() {
-  test('resizeImageForTest shrinks large image', () {
+  test('resizeImageForTest shrinks large image', () async {
     final image = img.Image(width: 2000, height: 2000); // Solid image
     final bytes = Uint8List.fromList(img.encodeJpg(image));
-    final resized = PdfReportGenerator.resizeImageForTest(bytes);
+    final resized = await PdfReportGenerator.resizeImageForTest(bytes);
     final decoded = img.decodeImage(resized)!;
     // Images should be resized down to the configured maximum dimension
     expect(decoded.width, lessThanOrEqualTo(96));
