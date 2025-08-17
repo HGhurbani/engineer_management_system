e37f2a1d80238c471017e4103c79d604609aa3bb
H_Ghurbani <poppinghazem3@gmail.com>
2025-08-17 11:28:37 -0700

    new

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,7 +18,7 @@ import path_provider_foundation
 import printing
 import share_plus
 import shared_preferences_foundation
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
